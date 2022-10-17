579d1fc884cac7200a5a3bd81741c9ffd465a952
PratikGarai <pratikgarai0208@gmail.com>
2022-10-17 05:44:22 +0530

    json response

--- auth/register.go
+++ auth/register.go
@@ -1,23 +1,31 @@
 package auth
 
 import (
+	"TeamVapp/AuthServer/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
+type UserInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserOutput struct {
 	Id       string `json:"id" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 func CreateUser(c *gin.Context) {
-	var newUser User
-	var err error = c.BindJSON(&newUser)
+	c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	c.Next()
+	var userInput UserInput
+	var err = c.BindJSON(&userInput)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, "Error parsing request body")
+		utils.HandleJsonParsingError(c, err)
 		return
 	}
-	c.JSON(http.StatusCreated, newUser)
+	c.JSON(http.StatusCreated, userInput)
 }
